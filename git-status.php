cacea74fbf863ceda89800919fad9c15f7236675
Nemanja Karadzic <manja@mineka.com>
2017-10-06 11:10:16 +0200

    Tabs to spaces conversion.

--- git-status.php
+++ git-status.php
@@ -7,42 +7,42 @@ echo " | " . str_pad("FOLDER", 50) . " | " . str_pad("ACTION", 20) . " |\n";
 echo " " . str_repeat("-", 77) . "\n";
 foreach ($dirs as $dir)
 {
-	$path = $root . "/" . $dir;
-	if (is_dir($path))
-	{
-		chdir($path);
-		if (is_dir($path . "/" . ".git"))
-		{
-			$msgs = array();
-			ob_start();
-			system("git status 2>&1", $return);
-			$output = ob_get_clean();
-			if(ob_get_length())
-			{
-				ob_end_clean();
-			}
-			if (strstr($output, "Changes not staged for commit"))
-			{
-				$msgs[] = "add";
-			}
-			if (strstr($output, "Changes to be commited"))
-			{
-				$msgs[] = "commit";
-			}
-			if (strstr($output, "Your branch is ahead"))
-			{
-				$msgs[] = "push";
-			}
-			if (strstr($output, "Your branch is behind"))
-			{
-				$msgs[] = "pull";
-			}
-			if (count($msgs))
-			{
-				$msg = join(", ", $msgs);
-				echo " | " . str_pad($dir, 50) . " | " . str_pad($msg, 20) . " |\n";
-				echo " " . str_repeat("-", 77) . "\n";
-			}
-		}
-	}
+    $path = $root . "/" . $dir;
+    if (is_dir($path))
+    {
+        chdir($path);
+        if (is_dir($path . "/" . ".git"))
+        {
+            $msgs = array();
+            ob_start();
+            system("git status 2>&1", $return);
+            $output = ob_get_clean();
+            if(ob_get_length())
+            {
+                ob_end_clean();
+            }
+            if (strstr($output, "Changes not staged for commit"))
+            {
+                $msgs[] = "add";
+            }
+            if (strstr($output, "Changes to be commited"))
+            {
+                $msgs[] = "commit";
+            }
+            if (strstr($output, "Your branch is ahead"))
+            {
+                $msgs[] = "push";
+            }
+            if (strstr($output, "Your branch is behind"))
+            {
+                $msgs[] = "pull";
+            }
+            if (count($msgs))
+            {
+                $msg = join(", ", $msgs);
+                echo " | " . str_pad($dir, 50) . " | " . str_pad($msg, 20) . " |\n";
+                echo " " . str_repeat("-", 77) . "\n";
+            }
+        }
+    }
 }
